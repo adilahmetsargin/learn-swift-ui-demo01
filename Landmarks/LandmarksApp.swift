7efc2ff0d70ea121c7d89ffb3ae5afe32c965919
Adil Ahmet Sargın <49733572+adilahmetsargin@users.noreply.github.com>
2024-01-01 15:47:04 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ahmet Sargın on 1.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
